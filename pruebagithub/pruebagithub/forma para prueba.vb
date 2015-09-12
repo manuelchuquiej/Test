cc5b13142d63d2f67b2c97827c85454594f5678b
Manuel Chuquiej <manuelchuquiej@hotmail.com>
2015-09-11 22:55:44 -0600

    se creo un nuevo formulario

---
+++ pruebagithub/pruebagithub/forma para prueba.vb
@@ -0,0 +1,7 @@
+﻿Public Class forma_para_prueba
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'cambio para git hub el cual se agrego como nuevo.' 
+
+    End Sub
+End Class
