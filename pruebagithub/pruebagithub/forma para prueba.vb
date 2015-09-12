2998e4cf9c48cfd79cca0ee72b28990c7e29506e
Manuel Chuquiej <manuelchuquiej@hotmail.com>
2015-09-12 00:01:40 -0600

    se mejoraron unas lineas

--- pruebagithub/pruebagithub/forma para prueba.vb
+++ pruebagithub/pruebagithub/forma para prueba.vb
@@ -3,5 +3,11 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'cambio para git hub el cual se agrego como nuevo.' 
 
+        'un nuevo cambio para ver como se arregla o se mejora'
+
+        'puede que funcione o se borre todo'
+
+        'hola'
+
     End Sub
 End Class
